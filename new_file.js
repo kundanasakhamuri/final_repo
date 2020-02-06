395b19d35b90b333a09d79c66429259d01cf2c23
kundanasakhamuri <60694277+kundanasakhamuri@users.noreply.github.com>
2020-02-06 19:14:01 +0530

    Update new_file.js

--- new_file.js
+++ new_file.js
@@ -4,6 +4,10 @@ var name = "John";
 	  	console.log("The name is : " + name);
 
 	  	var name = "Jane";
+		if(1==1)
+		{
+			console.log("test");
+		}
 
 	  	console.log("The name is : " +name);
 	})();
